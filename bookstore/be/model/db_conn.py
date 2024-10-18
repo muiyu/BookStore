1b905b90189492322f8b780020261f4942fab641
Azure-VM <riwego@qq.com>
2024-10-18 20:51:54 +0000

    Connect DB in be.model.database instead of be.model.store

--- bookstore/be/model/db_conn.py
+++ bookstore/be/model/db_conn.py
@@ -1,26 +1,26 @@
-from be.model import store
+from be.model import database
 
 
 class DBConn:
     def __init__(self):
-        self.conn = store.get_db_conn()
+        self.conn = database.get_db_conn()
 
     def user_id_exist(self, user_id):
-        result = self.conn.user_col.find_one({"user_id": user_id})
+        result = self.conn.user.find_one({"user_id": user_id})
         if result is None:
             return False
         else:
             return True
 
     def book_id_exist(self, store_id, book_id):
-        result = self.conn.store_col.find_one({"store_id": store_id, "books.book_id": book_id})
+        result = self.conn.store.find_one({"store_id": store_id, "book_id": book_id})
         if result is None:
             return False
         else:
             return True
 
     def store_id_exist(self, store_id):
-        result = self.conn.store_col.find_one({"store_id": store_id})
+        result = self.conn.user_store.find_one({"store_id": store_id})
         if result is None:
             return False
         else:
