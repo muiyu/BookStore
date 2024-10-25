5cec0cdac4cd366a3864647629140037c5cf8014
thirstylearning <129294556+thirstylearning@users.noreply.github.com>
2024-10-25 17:52:22 +0800

    Update db_conn.py using the class in the store.py

--- bookstore/be/model/db_conn.py
+++ bookstore/be/model/db_conn.py
@@ -1,27 +1,27 @@
-from be.model import database
+from be.model import store
 
 
 class DBConn:
     def __init__(self):
-        self.conn = database.get_db_conn()
+        self.conn = store.get_db_conn()
 
     def user_id_exist(self, user_id):
-        result = self.conn.user.find_one({"user_id": user_id})
-        if result is None:
+        cursor = self.conn['user'].find_one({"user_id": user_id})
+        if cursor is None:
             return False
         else:
             return True
 
     def book_id_exist(self, store_id, book_id):
-        result = self.conn.store.find_one({"store_id": store_id, "book_id": book_id})
-        if result is None:
+        cursor = self.conn['store'].find_one({'store_id': store_id, 'book_id': book_id})
+        if cursor is None:
             return False
         else:
             return True
 
     def store_id_exist(self, store_id):
-        result = self.conn.user_store.find_one({"store_id": store_id})
-        if result is None:
+        cursor = self.conn['user_store'].find_one({'store_id': store_id})
+        if cursor is None:
             return False
         else:
             return True
