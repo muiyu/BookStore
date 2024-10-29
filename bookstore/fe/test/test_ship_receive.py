4b5305eb9df7aeed48f631161267352711f30b58
Azure-VM <riwego@qq.com>
2024-10-29 21:13:44 +0000

    Fix issue#2

--- bookstore/fe/test/test_ship_receive.py
+++ bookstore/fe/test/test_ship_receive.py
@@ -3,8 +3,10 @@ import pytest
 from fe.access.buyer import Buyer
 from fe.test.gen_book_data import GenBook
 from fe.access.new_buyer import register_new_buyer
+from fe.access.new_seller import register_new_seller
 from fe.access.book import Book
 from fe.access.seller import Seller
+from fe.conf import Default_User_Funds
 import uuid
 
 
@@ -24,85 +26,87 @@ class TestShipReceive:
         self.seller_id = "test_payment_seller_id_{}".format(str(uuid.uuid1()))
         self.store_id = "test_payment_store_id_{}".format(str(uuid.uuid1()))
         self.buyer_id = "test_payment_buyer_id_{}".format(str(uuid.uuid1()))
-        self.password = self.seller_id
-
-        b = register_new_buyer(self.buyer_id, self.password)
-        self.buyer = b
-        gen_book = GenBook(self.seller_id, self.store_id)
-        self.seller = gen_book.seller
+        self.password = None
+        g = GenBook(self.seller_id, self.store_id)
+        self.seller = g.seller
+        self.buyer = register_new_buyer(self.buyer_id, self.buyer_id)
         self.buy_book_info_list = []
-        self.order_id = []
-        for i in range(2):
-            ok, buy_book_id_list = gen_book.gen(
-                non_exist_book_id=False, low_stock_level=False, max_book_count=2
-            )
-            self.buy_book_info_list.extend(gen_book.buy_book_info_list)
-            assert ok
+        self.order_id = None
+        
+        code, buy_book_id_list = g.gen(non_exist_book_id=False, low_stock_level=False, max_book_count=2)
+        assert code == True
+        self.buy_book_info_list.extend(g.buy_book_info_list)
 
-            code, order_id = b.new_order(self.store_id, buy_book_id_list)
-            self.order_id.append(order_id)
-            assert code == 200
-        self.total_price = 0
-        for item in self.buy_book_info_list:
-            book: Book = item[0]
-            num = item[1]
-            if book.price is None:
-                continue
-            else:
-                self.total_price = self.total_price + book.price * num
-        self.buyer.add_funds(self.total_price)
-        for i in range(1):
-            self.buyer.payment(self.order_id[i])
+        code, self.order_id = self.buyer.new_order(self.store_id, buy_book_id_list)
+        assert code == 200
+        assert self.order_id is not None
+        
         yield
 
     def test_ship_ok(self):
-        code = self.seller.ship_order(self.store_id, self.order_id[0])
+        self.buyer.add_funds(Default_User_Funds)
+        self.buyer.payment(self.order_id)
+        code = self.seller.ship_order(self.store_id, self.order_id)
         assert code == 200
 
     def test_receive_ok(self):
-        code = self.seller.ship_order(self.store_id, self.order_id[0])
+        self.buyer.add_funds(Default_User_Funds)
+        self.buyer.payment(self.order_id)
+        code = self.seller.ship_order(self.store_id, self.order_id)
         assert code == 200
-        code = self.buyer.receive_order(self.order_id[0])
+        code = self.buyer.receive_order(self.order_id)
         assert code == 200
 
     def test_error_store_id(self):
-        code = self.seller.ship_order(self.store_id + "_x", self.order_id[0])
+        self.buyer.add_funds(Default_User_Funds)
+        self.buyer.payment(self.order_id)
+        code = self.seller.ship_order(self.store_id + "_x", self.order_id)
         assert code != 200
 
     def test_error_order_id(self):
-        code = self.seller.ship_order(self.store_id, self.order_id[0] + "_x")
+        self.buyer.add_funds(Default_User_Funds)
+        self.buyer.payment(self.order_id)
+        code = self.seller.ship_order(self.store_id, self.order_id + "_x")
         assert code != 200
 
     def test_error_seller_id(self):
+        self.buyer.add_funds(Default_User_Funds)
+        self.buyer.payment(self.order_id)
         self.seller.seller_id = self.seller.seller_id + "_x"
-        code = self.seller.ship_order(self.store_id, self.order_id[0])
+        code = self.seller.ship_order(self.store_id, self.order_id)
         assert code != 200
 
     def test_error_buyer_id(self):
-        code = self.seller.ship_order(self.store_id, self.order_id[0])
+        self.buyer.add_funds(Default_User_Funds)
+        self.buyer.payment(self.order_id)
+        code = self.seller.ship_order(self.store_id, self.order_id)
         assert code == 200
         self.buyer.user_id = self.buyer.user_id + "_x"
-        code = self.buyer.receive_order(self.order_id[0])
+        code = self.buyer.receive_order(self.order_id)
         assert code != 200
 
     def test_ship_not_pay(self):
-        code = self.seller.ship_order(self.store_id, self.order_id[1])
+        code = self.seller.ship_order(self.store_id, self.order_id)
         assert code != 200
 
     def test_receive_not_ship(self):
-        code = self.buyer.receive_order(self.order_id[1])
+        code = self.buyer.receive_order(self.order_id)
         assert code != 200
 
     def test_repeat_ship(self):
-        code = self.seller.ship_order(self.store_id, self.order_id[0])
+        self.buyer.add_funds(Default_User_Funds)
+        self.buyer.payment(self.order_id)
+        code = self.seller.ship_order(self.store_id, self.order_id)
         assert code == 200
-        code = self.seller.ship_order(self.store_id, self.order_id[0])
+        code = self.seller.ship_order(self.store_id, self.order_id)
         assert code != 200
 
     def test_repeat_receive(self):
-        code = self.seller.ship_order(self.store_id, self.order_id[0])
+        self.buyer.add_funds(Default_User_Funds)
+        self.buyer.payment(self.order_id)
+        code = self.seller.ship_order(self.store_id, self.order_id)
         assert code == 200
-        code = self.buyer.receive_order(self.order_id[0])
+        code = self.buyer.receive_order(self.order_id)
         assert code == 200
-        code = self.buyer.receive_order(self.order_id[0])
+        code = self.buyer.receive_order(self.order_id)
         assert code != 200
