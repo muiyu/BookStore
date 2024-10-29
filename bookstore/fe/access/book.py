eb8a080d2f30b068082b5d06bfde9805a7987320
Azure-VM <riwego@qq.com>
2024-10-29 21:39:37 +0000

    Complete: Now test would use the data from MongoDB which is provided by book.db/book_lx.db

--- bookstore/fe/access/book.py
+++ bookstore/fe/access/book.py
@@ -1,11 +1,13 @@
 import os
-import sqlite3 as sqlite
 import random
 import base64
-import simplejson as json
-
+import pymongo
 
 class Book:
+    def __init__(self):
+        self.tags = []
+        self.pictures = []
+
     id: str
     title: str
     author: str
@@ -15,7 +17,6 @@ class Book:
     pub_year: str
     pages: int
     price: int
-    currency_unit: str
     binding: str
     isbn: str
     author_intro: str
@@ -24,61 +25,49 @@ class Book:
     tags: [str]
     pictures: [bytes]
 
-    def __init__(self):
-        self.tags = []
-        self.pictures = []
-
 
 class BookDB:
-    def __init__(self, large: bool = False):
-        parent_path = os.path.dirname(os.path.dirname(__file__))
-        self.db_s = os.path.join(parent_path, "data/book.db")
-        self.db_l = os.path.join(parent_path, "data/book_lx.db")
-        if large:
-            self.book_db = self.db_l
-        else:
-            self.book_db = self.db_s
+    __socket = None
+    __db = None
 
+    def __init__(self, not_used_param: bool = False):
+        ### 注释行专为远程数据库使用 ###
+        self.socket = pymongo.MongoClient(os.getenv('MONGODB_API'), server_api=pymongo.server_api.ServerApi('1'))
+        # self.socket = pymongo.MongoClient('mongodb://localhost:27017')
+        self.db = self.socket['bookstore']
+
+        try:
+            self.db['books'].create_index([("id", pymongo.ASCENDING)])
+        except:
+            pass
+        
     def get_book_count(self):
-        conn = sqlite.connect(self.book_db)
-        cursor = conn.execute("SELECT count(id) FROM book")
-        row = cursor.fetchone()
-        return row[0]
+        return self.db['books'].count_documents({})
 
     def get_book_info(self, start, size) -> [Book]:
         books = []
-        conn = sqlite.connect(self.book_db)
-        cursor = conn.execute(
-            "SELECT id, title, author, "
-            "publisher, original_title, "
-            "translator, pub_year, pages, "
-            "price, currency_unit, binding, "
-            "isbn, author_intro, book_intro, "
-            "content, tags, picture FROM book ORDER BY id "
-            "LIMIT ? OFFSET ?",
-            (size, start),
-        )
-        for row in cursor:
+        cursor = self.db['books'].find().skip(start).limit(size)
+        for doc in cursor:
             book = Book()
-            book.id = row[0]
-            book.title = row[1]
-            book.author = row[2]
-            book.publisher = row[3]
-            book.original_title = row[4]
-            book.translator = row[5]
-            book.pub_year = row[6]
-            book.pages = row[7]
-            book.price = row[8]
+            book.id = doc.get("id")
+            book.title = doc.get("title")
+            book.author = doc.get("author")
+            book.publisher = doc.get("publisher")
+            book.original_title = doc.get("original_title")
+            book.translator = doc.get("translator")
+            book.pub_year = doc.get("pub_year")
+            book.pages = doc.get("pages")
+            book.price = doc.get("price")
 
-            book.currency_unit = row[9]
-            book.binding = row[10]
-            book.isbn = row[11]
-            book.author_intro = row[12]
-            book.book_intro = row[13]
-            book.content = row[14]
-            tags = row[15]
+            book.currency_unit = doc.get("currency_unit")
+            book.binding = doc.get("binding")
+            book.isbn = doc.get("isbn")
+            book.author_intro = doc.get("author_intro")
+            book.book_intro = doc.get("book_intro")
+            book.content = doc.get("content")
+            tags = doc.get("tags")
 
-            picture = row[16]
+            picture = doc.get("picture")
 
             for tag in tags.split("\n"):
                 if tag.strip() != "":
@@ -88,5 +77,4 @@ class BookDB:
                     encode_str = base64.b64encode(picture).decode("utf-8")
                     book.pictures.append(encode_str)
             books.append(book)
-
         return books
